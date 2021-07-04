425e191c04470de84406ec4ad1559efe1a8706e4
Kwstas <kostasntnt@hotmail.gr>
2021-07-04 21:40:53 +0300

    lektikos analuths,compiler.h working

---
+++ compiler.h
@@ -0,0 +1,53 @@
+
+// termatikoi xarakthres
+#define T_PROGRAM 1
+#define T_FUNCTION 2
+#define T_VARS 3
+#define T_CHAR 4
+#define T_INTEGER 5
+#define T_END_FUNCTION 6
+#define T_RETURN 7
+#define T_STARTMAIN 8
+#define T_ENDMAIN 9
+#define T_WHILE 10
+#define T_ENDWHILE 11
+#define T_FOR 12
+#define T_TO 13
+#define T_STEP 14
+#define T_ENDFOR 15
+#define T_IF 16
+#define T_THEN 17
+#define T_ELSEIF 18
+#define T_ENDIF 19
+#define T_SWITCH 20
+#define T_CASE 21
+#define T_DEFAULT 22
+#define T_ENDSWITCH 23
+#define T_BREAK 24
+
+
+//mh termatikoi xarakthres
+// telestes
+#define T_OROP 25
+#define T_ANDOP 26
+#define T_EQUOP 27
+#define T_RELOP 28
+#define T_ADDOP 29
+#define T_MULOP 30
+
+#define T_ID 70
+#define T_DIGIT 71
+#define T_NUM 72
+#define T_SCONST 73 //string
+// lexikoi telestes
+#define T_LPAREN 31
+#define T_RPAREN 32
+#define T_SEMI 33
+#define T_COMMA 34
+#define T_ASSIGN 35
+#define T_LBRACK 36
+#define T_RBRACK 37
+#define T_LBRACE 38
+#define T_RBRACE 39
+
+#define T_EOF 0
